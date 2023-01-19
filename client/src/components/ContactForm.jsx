e2e898ea7eb94e6c2db12f413ae88aa4d01f85b7
Aaron Russell <cpl.adrusselljr@gmail.com>
2023-01-19 11:48:57 -0500

    trying to get contactForm to work

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+// variables
+const SERVICE_ID = "service_8xc4pkg"
+const TEMPLATE_ID = "template_f4u1prw"
+const PUBLIC_KEY = "HGkCuXmWGyc6p4Vs6"
 
 const ContactForm = () => {
+    const form = useRef()
+
+    const sendEmail = e => {
+        e.preventDefault()
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text)
+            }, (error) => {
+                console.log(error.text)
+            })
+    }
 
     return (
         <div>
-            <div>
-                <label>Name: </label>
-                <input type='text' placeholder='Name' style={{ marginLeft: "40px", width: "250px" }} />
-            </div>
-            <div>
-                <label>Email: </label>
-                <input type='email' placeholder='Email address' style={{ marginLeft: "45px", marginTop: "50px", width: "250px" }} />
-            </div>
-            <div>
-                <label>Subject: </label>
-                <input type='text' placeholder='Subject' style={{ marginLeft: "25px", marginTop: "50px", width: "250px" }} />
-            </div>
-            <div>
-                <label>Message:</label>
-                <textarea rows={ 3 } placeholder='Message' style={{ marginLeft: "15px", marginTop: "50px", width: "250px", height: "100px" }} />
-            </div>
-            <button className='submit-btn' type='submit'>Submit</button>
+            <form ref={ form } onSubmit={ sendEmail }>
+                <div>
+                    <label>Name: </label>
+                    <input name='name' type='text' placeholder='Name' style={{ marginLeft: "40px", width: "250px" }} />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input name='email' type='email' placeholder='Email address' style={{ marginLeft: "45px", marginTop: "50px", width: "250px" }} />
+                </div>
+                <div>
+                    <label>Subject: </label>
+                    <input name='subject' type='text' placeholder='Subject' style={{ marginLeft: "25px", marginTop: "50px", width: "250px" }} />
+                </div>
+                <div>
+                    <label>Message:</label>
+                    <textarea name="message" rows={ 3 } placeholder='Message' style={{ marginLeft: "15px", marginTop: "50px", width: "250px", height: "100px" }} />
+                </div>
+                <button value="Send" className='submit-btn' type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
